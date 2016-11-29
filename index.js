9b965a8ee1b9da49bc622121f4d860c0f28a3889
Mitchell de Rijcke <mitchell@de.rijcke.org>
2016-11-29 16:26:16 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require("express")
+const fs = require("fs")
+const pug = require("pug")
+
+let app = express()
+
+let config = {
+	blocks: [
+		{
+			module: "nav",
+			siteName: "Blocks",
+			nav: ["Home", "Contact"],
+			right: ["Test"]
+		},
+		{
+			module: "text",
+			container: true,
+			content: "Hello and welcome to the experiment!"
+		},
+		{
+			module: "html",
+			container: true,
+			content: "<hr>"
+		},
+		{
+			module: "grid",
+			blocks: [
+				{
+					module: "text",
+					size: 0.5,
+					content: "Look at that nice form! »"
+				},
+				{
+					module: "form",
+					size: 0.5,
+					form: [
+						{
+							type: "message",
+							severity: "warning",
+							text: "Do not enter fake details!"
+						},
+						{
+							type: "text",
+							name: "name",
+							label: "Full Name"
+						},
+						{
+							type: "text",
+							name: "email",
+							label: "E-mail address"
+						},
+						{
+							type: "textarea",
+							name: "message",
+							label: "Message"
+						},
+						{
+							type: "checkbox",
+							name: "subscribe",
+							label: "Subscribe to our newsletter?"
+						}
+					],
+					submitText: "Send"
+				}
+			]
+		},
+		{
+			module: "footer",
+			copyright: "Copyright © Test Company 2016"
+		}
+	]
+}
+
+function render(blocks) {
+	let html = []
+
+	for (let block of blocks) {
+		if (block.module == "grid") {
+			// TODO: OOP so the template calls .render()
+			block.renders = render(block.blocks)
+		}
+
+		let htmlCode = pug.render(fs.readFileSync("modules/" + block.module + ".pug", "utf8"), block)
+
+		if (block.container) {
+			htmlCode = "<div class=\"container\">" + htmlCode + "</div>"
+		}
+
+		html.push(htmlCode)
+	}
+
+	return html
+}
+
+app.get("/", (req, res) => {
+	res.header("Content-Type", "text/html; charset=utf-8")
+	res.send(pug.render(fs.readFileSync("base.pug", "utf-8"), {
+		blocks: render(config.blocks)
+	}))
+})
+
+app.listen(1337)
